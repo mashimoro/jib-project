63a47e20e259105d027e2de8b019b1220210bde9
mashimoro <putkhiew@gmail.com>
2020-08-08 14:08:27 +0700

    add pip install pytest-django

--- safety/safety/settings/base.py
+++ safety/safety/settings/base.py
@@ -134,7 +134,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL ='/media/'
-
 MEDIA_ROOT= Path(BASE_DIR) / 'media'
 
 
--- safety/safety/settings/local.py
+++ safety/safety/settings/local.py
@@ -1,2 +1,11 @@
 from .base import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MEDIA_URL ='/media/'
+MEDIA_ROOT= Path(BASE_DIR) / 'media'
