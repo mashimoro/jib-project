d12ef9ac6747f4fb110c305d067cfa26a8bc1e34
mashimoro <putkhiew@gmail.com>
2020-08-08 10:29:48 +0700

    test mock function

---
+++ getRendom.py
@@ -0,0 +1,4 @@
+import random
+
+def get_rendom_number_plus_one():
+  return random.randint(1 ,10) + 1
--- try_mock.py
+++ try_mock.py
@@ -1,18 +1,26 @@
-import random
 import unittest
+from unittest.mock import patch
+import getRendom  
 
 class TetsRandom(unittest.TestCase):
+  @patch('getRendom.random.randint')
+  def test_it_should_call_andint_eith_1_and_10(self, mock):
+    getRendom.get_rendom_number_plus_one()
+    mock.assert_called_once_with(1,10)
+
+  @patch('getRendom.random.randint')
+  def test_it_should_get_4_when_random_get_3(self, mock):
+    # mock.return_value = 3   stap mock
+    mock.return_value = 3  
+    result = getRendom.get_rendom_number_plus_one()
+    self.assertEqual(result,4)
+   
   def test_it_should_get_3(self):
     while True:
-      result = get_rendom_number_plus_one()
+      result = getRendom.get_rendom_number_plus_one()
       if result == 3:
         break
-    self.assertEqual(result ,3)
-  
-  
-
-def get_rendom_number_plus_one():
-  return random.randint(1 ,10) + 1
+    self.assertEqual(result ,3) 
 
 # print(get_rendom_number_plus_one())
 unittest.main()
