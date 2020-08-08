899f6e7d71dc08aa0165c97d71d1467e321f8e8f
mashimoro <putkhiew@gmail.com>
2020-08-08 11:15:21 +0700

    mock response

--- safety/covid19_report/test/tests_view.py
+++ safety/covid19_report/test/tests_view.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
+from unittest.mock import patch
 
+# from v
 # Create your tests here.
 class TestCovid19ReportView(TestCase):
   def test_view_should_be_accesible(self):
     response = self.client.get('/covid19-reports/')
     self.assertEqual(response.status_code,200)
+
+  @patch('covid19_report.views.requests.get')
+  def test_view_shound_call_covidApi(self,mock):
+    self.client.get('/covid19-reports/')
+    mock.assert_called_once_with('https://covid19.th-stat.com/api/open/today')
+    # response = self.client.get('/covid19-reports/')
+    # print(f'response=={response}')
 
--- safety/covid19_report/views.py
+++ safety/covid19_report/views.py
@@ -1,6 +1,7 @@
 from django.views import View
 from django.http import HttpResponse
-
+import requests
 class Covid19ReportView(View):
   def get(self,request):
+    requests.get('https://covid19.th-stat.com/api/open/today')
     return HttpResponse()
