5616dba3f004d8a1f598dff3b3b8c8b822cfd3bc
mashimoro <putkhiew@gmail.com>
2020-08-08 11:21:47 +0700

    stap api

--- safety/covid19_report/test/tests_view.py
+++ safety/covid19_report/test/tests_view.py
@@ -4,7 +4,9 @@ from unittest.mock import patch
 # from v
 # Create your tests here.
 class TestCovid19ReportView(TestCase):
-  def test_view_should_be_accesible(self):
+  #stap ไม่ให้ test นี้ออก api จริง
+  @patch('covid19_report.views.requests.get')
+  def test_view_should_be_accesible(self,_):
     response = self.client.get('/covid19-reports/')
     self.assertEqual(response.status_code,200)
 
